1a83d58be37ce09d9baf7e4ca47df3459c0cc74c
ElisaROGGE <elisa.rogge59@gmail.com>
2024-10-04 09:19:50 +0200

    fix: userService types

--- src/services/user/user.service.ts
+++ src/services/user/user.service.ts
@@ -1,5 +1,14 @@
 import { fetchData } from "../../utils/fetch";
 
-export const login = (data) => fetchData(`http://localhost:3001/api/v1/user/login`, 'POST', data)
-export const getUser = (token) => fetchData(`http://localhost:3001/api/v1/user/profile`, 'POST', null, token)
-export const updateUser = (data, token) => fetchData(`http://localhost:3001/api/v1/user/profile`, 'PUT', data, token)
+interface ILogin {
+    email: string
+    password: string
+}
+interface IUser{
+    lastName: string
+    firstName: string
+}
+
+export const login = (data: ILogin) => fetchData(`http://localhost:3001/api/v1/user/login`, 'POST', data)
+export const getUser = (token: string) => fetchData(`http://localhost:3001/api/v1/user/profile`, 'POST', null, token)
+export const updateUser = (data: IUser, token: string) => fetchData(`http://localhost:3001/api/v1/user/profile`, 'PUT', data, token)
